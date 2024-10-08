040f90223df2999f37f35ab79fbd8051c3334caf
Kabelo Peele <kabelom.p@live.com>
2024-10-08 07:39:30 +0200

    Added props of 'limit' and 'showButton'

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -4,6 +4,15 @@ import JobListing from './JobListing.vue';
 import { ref } from 'vue';
 
 const jobs = ref(JobData);
+
+defineProps({
+    limit: Number,
+    showButton: {
+        type: Boolean,
+        default: false
+    }
+});
+
 </script>
 
 <template>
@@ -13,8 +22,13 @@ const jobs = ref(JobData);
                 Browse Jobs
             </h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
+    </section>
+
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+        <a href="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View
+            All Jobs</a>
     </section>
 </template>
