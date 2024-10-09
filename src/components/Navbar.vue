91aabba4add667be958da0cfc980a2da71ce2f48
Kabelo Peele <kabelom.p@live.com>
2024-10-09 09:19:29 +0200

    Created 'JobView' component, and updated paths to show '/jobs/:id' instead of '/job/:id'

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,7 @@ const isActiveLink = (routePath) => {
             <div class="flex h-20 items-center justify-between">
                 <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
                     <!-- Logo -->
-                    <RouterLink class="flex flex-shrink-0 items-center mr-4" to="index.html">
+                    <RouterLink class="flex flex-shrink-0 items-center mr-4" to="/">
                         <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
                         <span class="hidden md:block text-white text-2xl font-bold ml-2">Vue Jobs</span>
                     </RouterLink>
