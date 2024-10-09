4f885a66069ef70f0bc6daca3b03f93d1cf070ae
Kabelo Peele <kabelom.p@live.com>
2024-10-09 09:08:19 +0200

    Added 'RouterLink' to 'JobListings', created 'NotFoundView' 404 component and path registry

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -2,6 +2,7 @@
 import JobData from '@/jobs.json';
 import JobListing from './JobListing.vue';
 import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
 
 const jobs = ref(JobData);
 
@@ -28,7 +29,7 @@ defineProps({
     </section>
 
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-        <a href="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View
-            All Jobs</a>
+        <RouterLink to="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View
+            All Jobs</RouterLink>
     </section>
 </template>
